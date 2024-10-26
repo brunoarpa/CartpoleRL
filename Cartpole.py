d9558e77ca60d37f99fd100882692365c5ce08cb
Bruno Arpa <bruno.arpa.n@gmail.com>
2024-10-26 14:53:33 +0200

    processing and dependencies Cartpole.py

--- Cartpole.py
+++ Cartpole.py
@@ -1,2 +1,15 @@
-print("isdfhifsdh")
-print("sssdg?")
+import math
+import random
+import gymnasium
+import torch
+import time
+
+# Make the cartpole environment from gymnasium  :)))
+env = gymnasium.make("CartPole-v1")
+# Use GPU for better processing
+device = torch.device(
+    "mps" if torch.backends.mps.is_available() else # Apple 
+    "cuda" if torch.cuda.is_available() else    
+    "cpu"
+)
+
