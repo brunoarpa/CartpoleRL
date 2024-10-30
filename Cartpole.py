4d2e0f94424c4aa458232af871fbd2acb3b646c9
Bruno Arpa <bruno.arpa.n@gmail.com>
2024-10-30 18:19:46 +0100

    NN for the Cartpole.py

--- Cartpole.py
+++ Cartpole.py
@@ -13,3 +13,16 @@ device = torch.device(
     "cpu"
 )
 
+# Create Neural Network
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions): 
+        super(DQN, self).__init__() #Initializes parent class nn.Module
+        self.layer1 = nn.Linear(n_observations, 128) # Each layer (inputs to layer, outputs of layer)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    def forward(self, x): # Called with either one element to determine next action, or a batch during optimization
+        x = F.relu(self.layer1(x)) # Going through each layer
+        x = F.relu(self.layer2(x))
+        return self.layer3(x) # Returns tensor([[left0exp,right0exp]...])
